d9534b24e92c27a55c845153fa72ed68859eb6ff
Kingsley Quarshie <kingsley.quarshie.01@gmail.com>
2026-09-24 10:28:04 +0000

    Final Commit

--- backend/src/utils/AppError.js
+++ backend/src/utils/AppError.js
@@ -1,7 +1,7 @@
-export class AppError extends Error {
-  constructor(message, statusCode = 500) {
+export class AppError extends Error { 
+  constructor(message, statusCode = 500) { 
     super(message);
-    this.statusCode = statusCode;
+    this.statusCode = statusCode; 
     this.isOperational = true;
     Error.captureStackTrace(this, this.constructor);
   }
